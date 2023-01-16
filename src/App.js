4db6b2e3df9f1c18a2dbb90be846abe7ee9f2a2c
Sebastian Steins <sebastian.steins@gmail.com>
2023-01-16 15:50:35 +0100

    API call

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+
+  const onSubmit = () => {
+    const url = process.env.REACT_APP_API_ENDPOINT + "/?name=API"
+    fetch(url).then(response => {alert(response)});
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br />
-          REACT_APP_ABC: {process.env.REACT_APP_ABC}
+          REACT_APP_API_ENDPOINT: {process.env.REACT_APP_API_ENDPOINT}
         </p>
+
+        <p><label for="name"><input type="text" id="name" /></label></p>
+        <p><label for="submit"><button id="submit" onClick={onSubmit}>OK</button></label></p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
