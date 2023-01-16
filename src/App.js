ca8572751d039cc6bdf414eebd0c0374dca8294b
Sebastian Steins <sebastian.steins@gmail.com>
2023-01-16 16:17:45 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -9,30 +9,19 @@ function App() {
   const onSubmit = () => {
     console.log(process.env.REACT_APP_API_ENDPOINT)
     const url = process.env.REACT_APP_API_ENDPOINT + "/?name=API"
-    fetch(url).then(response => {alert(response); console.log(response.body)});
-    fetch(url).then(response => (response.text()).then(data=>{console.log(data); alert(data)}));
+    fetch(url).then(response => (response.text()).then(data=>{ alert(data)}));
   }
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.<br />
-          REACT_APP_API_ENDPOINT: {process.env.REACT_APP_API_ENDPOINT}
-        </p>
+
 
         <p><label for="name"><input type="text" id="name" /></label></p>
         <p><label for="submit"><button id="submit" onClick={onSubmit}>OK</button></label></p>
 
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
