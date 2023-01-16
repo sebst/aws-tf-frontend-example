5c8474c1ca092e320e75608dac7739a4f4ae29fb
Sebastian Steins <sebastian.steins@gmail.com>
2023-01-16 16:08:32 +0100

    ...

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const onSubmit = () => {
     console.log(process.env.REACT_APP_API_ENDPOINT)
     const url = process.env.REACT_APP_API_ENDPOINT + "/?name=API"
-    fetch(url).then(response => {alert(response); console.log(response)});
+    fetch(url).then(response => {alert(response); console.log(response.body)});
   }
 
   return (
